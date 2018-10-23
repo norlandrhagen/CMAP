e76173805a0facd9d20c87554d0d829b71e8bac7
Raphael Hagen <norlandrhagen@gmail.com>
2018-10-22 18:16:14 -0700

    updated config

--- conf.py
+++ conf.py
@@ -76,6 +76,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
